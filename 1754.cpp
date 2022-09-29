633bdc5fc17db9a7c4cec0d077f1154f6ffe4797
B. Anitya Aniruddha <b.a.aniruddha@proton.me>
2022-09-29 21:49:08 +0600

    Update 1754.cpp

--- 1754.cpp
+++ 1754.cpp
@@ -1,1 +1,18 @@
 //https://cses.fi/problemset/task/1754
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int n, a, b;
+    cin >> n;
+    while (n--) {
+        cin >> a >> b;
+        if ((a + b) % 3 == 0 && a <= (2 * b) && b <= (2 * a))
+            cout << "YES" << "\n";
+        else
+            cout << "NO" << "\n";
+    }
+    return 0;
+}
